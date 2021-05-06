7797193c489580a96569b6d66a04cb3147ea293a
senoralikz <alexjm825@gmail.com>
2021-05-06 10:20:51 -0400

    added login buttons and text box

---
+++ src/components/Login.js
@@ -0,0 +1,30 @@
+import React, { useRef } from "react";
+import { Container, Form, Button } from "react-bootstrap";
+
+export default function Login({ onIdSubmit }) {
+  const idRef = useRef();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    onIdSubmit(idRef.current.value);
+  }
+
+  return (
+    <Container
+      className="align-items-center d-flex"
+      style={{ height: "100vh" }}
+    >
+      <Form className="w-100" onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label>Enter Your ID</Form.Label>
+          <Form.Control type="text" ref={idRef} required />
+        </Form.Group>
+        <Button type="submit" className="m-2">
+          Login
+        </Button>
+        <Button variant="secondary">Create A New ID</Button>
+      </Form>
+    </Container>
+  );
+}
